d04d5c34106fdd30cabd58273a0c83efedf2a0b5
m3gagluk <15657215+m3gagluk@users.noreply.github.com>
2024-02-07 00:44:08 +0300

    Implemented the Varjo-specific Process ID hook

--- Interface/VarjoInterface.cs
+++ Interface/VarjoInterface.cs
@@ -1,140 +1,132 @@
 ﻿using System.IO;
 using System.Runtime.InteropServices;
 
-namespace ResoniteVarjoEye;
-
-//Varjo's structs used with both native library and companion
-[StructLayout(LayoutKind.Sequential)]
-public struct Vector
+namespace ResoniteNativeVarjoIntegration
 {
-    public double x;
-    public double y;
-    public double z;
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct GazeRay
-{
-    public Vector origin;   //!< Origin of the ray.
-    public Vector forward;  //!< Direction of the ray.
-}
-
-public enum GazeStatus : long
-{
-    Invalid = 0,
-    Adjust = 1,
-    Valid = 2
-}
-
-public enum GazeEyeStatus : long
-{
-    Invalid = 0,
-    Visible = 1,
-    Compensated = 2,
-    Tracked = 3
-}
-
-
-[StructLayout(LayoutKind.Sequential)]
-public struct GazeData
-{
-    public GazeRay leftEye;                 //!< Left eye gaze ray.
-    public GazeRay rightEye;                //!< Right eye gaze ray.
-    public GazeRay gaze;                    //!< Normalized gaze direction ray.
-    public double focusDistance;            //!< Estimated gaze direction focus point distance.
-    public double stability;                //!< Focus point stability.
-    public long captureTime;                //!< Varjo time when this data was captured, see varjo_GetCurrentTime()
-    public GazeEyeStatus leftStatus;        //!< Status of left eye data.
-    public GazeEyeStatus rightStatus;       //!< Status of right eye data.
-    public GazeStatus status;               //!< Tracking main status.
-    public long frameNumber;                //!< Frame number, increases monotonically.
-    public double leftPupilSize;            //!< Normalized [0..1] left eye pupil size.
-    public double rightPupilSize;           //!< Normalized [0..1] right eye pupil size.
-}
-
-
-[StructLayout(LayoutKind.Sequential)]
-public struct EyeMeasurements
-{
-    public long frameNumber;                    //!< Frame number, increases monotonically.
-    public long captureTime;                    //!< Varjo time when this data was captured, see varjo_GetCurrentTime()
-    public float interPupillaryDistanceInMM;    //!< Estimated IPD in millimeters
-    public float leftPupilIrisDiameterRatio;    //!< Ratio between left pupil and left iris.
-    public float rightPupilIrisDiameterRatio;   //!< Ratio between right pupil and right iris.
-    public float leftPupilDiameterInMM;         //!< Left pupil diameter in mm
-    public float rightPupilDiameterInMM;        //!< Right pupil diameter in mm
-    public float leftIrisDiameterInMM;          //!< Left iris diameter in mm
-    public float rightIrisDiameterInMM;         //!< Right iris diameter in mm
-    public float leftEyeOpenness;               //!< Left Eye Openness
-    public float rightEyeOpenness;              //!< Right Eye Openness
-}
-
-
-[StructLayout(LayoutKind.Sequential)]
-public struct GazeCalibrationParameter
-{
-    [MarshalAs(UnmanagedType.LPStr)] public string key;
-    [MarshalAs(UnmanagedType.LPStr)] public string value;
-}
-
-public enum GazeCalibrationMode
-{
-    Legacy,
-    Fast
-};
-
-public enum GazeOutputFilterType
-{
-    None,
-    Standard
-}
-
-public enum GazeOutputFrequency
-{
-    MaximumSupported,
-    Frequency100Hz,
-    Frequency200Hz
-}
-
-public enum GazeEyeCalibrationQuality
-{
-    Invalid = 0,
-    Low = 1,
-    Medium = 2,
-    High = 3
-}
-
-[StructLayout(LayoutKind.Sequential)]
-public struct GazeCalibrationQuality
-{
-    public GazeEyeCalibrationQuality left;
-    public GazeEyeCalibrationQuality right;
-}
-
-
-public abstract class VarjoInterface
-{
-    protected GazeData gazeData;
-    protected EyeMeasurements eyeMeasurements;
-
-    public GazeData GetGazeData()
+    //Varjo's structs used with both native library and companion
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vector
     {
-        return gazeData;
+        public double x;
+        public double y;
+        public double z;
     }
 
-    public EyeMeasurements GetEyeMeasurements()
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GazeRay
     {
-        return eyeMeasurements;
+        public Vector origin;   //!< Origin of the ray.
+        public Vector forward;  //!< Direction of the ray.
     }
 
-    public abstract void Teardown();
-    public abstract bool Initialize();
-    public abstract void Update();
-    public abstract string GetName();
-
-    protected bool VarjoAvailable()
+    public enum GazeStatus : long
     {
-        // totally not how the official Varjo library works under the hood
-        return File.Exists("\\\\.\\pipe\\Varjo\\InfoService");
+        Invalid = 0,
+        Adjust = 1,
+        Valid = 2
     }
+
+    public enum GazeEyeStatus : long
+    {
+        Invalid = 0,
+        Visible = 1,
+        Compensated = 2,
+        Tracked = 3
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GazeData
+    {
+        public GazeRay leftEye;                 //!< Left eye gaze ray.
+        public GazeRay rightEye;                //!< Right eye gaze ray.
+        public GazeRay gaze;                    //!< Normalized gaze direction ray.
+        public double focusDistance;            //!< Estimated gaze direction focus point distance.
+        public double stability;                //!< Focus point stability.
+        public long captureTime;                //!< Varjo time when this data was captured, see varjo_GetCurrentTime()
+        public GazeEyeStatus leftStatus;        //!< Status of left eye data.
+        public GazeEyeStatus rightStatus;       //!< Status of right eye data.
+        public GazeStatus status;               //!< Tracking main status.
+        public long frameNumber;                //!< Frame number, increases monotonically.
+        public double leftPupilSize;            //!< Normalized [0..1] left eye pupil size.
+        public double rightPupilSize;           //!< Normalized [0..1] right eye pupil size.
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct EyeMeasurements
+    {
+        public long frameNumber;                    //!< Frame number, increases monotonically.
+        public long captureTime;                    //!< Varjo time when this data was captured, see varjo_GetCurrentTime()
+        public float interPupillaryDistanceInMM;    //!< Estimated IPD in millimeters
+        public float leftPupilIrisDiameterRatio;    //!< Ratio between left pupil and left iris.
+        public float rightPupilIrisDiameterRatio;   //!< Ratio between right pupil and right iris.
+        public float leftPupilDiameterInMM;         //!< Left pupil diameter in mm
+        public float rightPupilDiameterInMM;        //!< Right pupil diameter in mm
+        public float leftIrisDiameterInMM;          //!< Left iris diameter in mm
+        public float rightIrisDiameterInMM;         //!< Right iris diameter in mm
+        public float leftEyeOpenness;               //!< Left Eye Openness
+        public float rightEyeOpenness;              //!< Right Eye Openness
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GazeCalibrationParameter
+    {
+        [MarshalAs(UnmanagedType.LPStr)] public string key;
+        [MarshalAs(UnmanagedType.LPStr)] public string value;
+    }
+
+    public enum GazeCalibrationMode
+    {
+        Legacy,
+        Fast
+    };
+
+    public enum GazeOutputFilterType
+    {
+        None,
+        Standard
+    }
+
+    public enum GazeOutputFrequency
+    {
+        MaximumSupported,
+        Frequency100Hz,
+        Frequency200Hz
+    }
+
+    public enum GazeEyeCalibrationQuality
+    {
+        Invalid = 0,
+        Low = 1,
+        Medium = 2,
+        High = 3
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct GazeCalibrationQuality
+    {
+        public GazeEyeCalibrationQuality left;
+        public GazeEyeCalibrationQuality right;
+    }
+
+
+    public enum VarjoPropertyKey
+    {
+        Invalid = 0x0,
+        UserPresence = 0x2000,
+        GazeCalibrating = 0xA000,
+        GazeCalibrated = 0xA001,
+        GazeCalibrationQuality = 0xA002,
+        GazeAllowed = 0xA003,
+        GazeEyeCalibrationQuality_Left = 0xA004,
+        GazeEyeCalibrationQuality_Right = 0xA005,
+        GazeIPDEstimate = 0xA010,
+        HMDConnected = 0xE001,
+        HMDProductName = 0xE002,
+        HMDSerialNumber = 0xE003,
+        MRAvailable = 0xD000
+    }
+
 }
--- Interface/VarjoNativeInterface.cs
+++ Interface/VarjoNativeInterface.cs
@@ -1,112 +1,233 @@
-﻿using Elements.Core;
+﻿using EasyHook;
+using Elements.Core;
 using FrooxEngine;
+using ResoniteModLoader;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading;
 
-namespace ResoniteVarjoEye;
-
-public class VarjoNativeInterface : VarjoInterface
+namespace ResoniteNativeVarjoIntegration
 {
-    private IntPtr _session;
-
-    public override bool Initialize()
+    class VarjoNativeInterface
     {
-        //if (!VarjoAvailable())
-        //{
-        //    UniLog.Error("Varjo headset isn't detected");
-        //    return false;
-        //}
-        LoadLibrary();
-        _session = varjo_SessionInit();
-        if (_session == IntPtr.Zero)
+        private IntPtr _session;
+        protected GazeData gazeData;
+        protected EyeMeasurements eyeMeasurements;
+
+        // windows consts because I'm too lazy to find references
+        private const int GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS = 0x00000004;
+        private const int GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT = 0x00000002;
+
+
+
+        #region Lifetime methods (Init, Update, Teardown)
+        public bool Initialize()
         {
+            if (!VarjoAvailable())
+            {
+                ResoniteMod.Error("Varjo headset isn't detected");
+                return false;
+            }
+            bool libraryLoaded = false;
+            using (var hook = EasyHook.LocalHook.Create(EasyHook.LocalHook.GetProcAddress("kernel32.dll", "GetCurrentProcessId"), new GetCurrentProcessIdDelegate(GetCurrentProcessId_Hook), this))
+            {
+                hook.ThreadACL.SetExclusiveACL(new Int32[] { });
+                // Call the evil Varjoe code with thw evil fake processId hook
+                libraryLoaded = LoadLibrary();
+
+                if (libraryLoaded)
+                {
+                    _session = varjo_SessionInit();
+                    // Aand we don't need the evil hook anymore
+                    hook.ThreadACL.SetInclusiveACL(new Int32[] { });
+
+                    if (_session == IntPtr.Zero)
+                    {
+                        ResoniteMod.Warn("Varjo init failed");
+                        return false;
+                    }
+                    if (!varjo_IsGazeAllowed(_session))
+                    {
+                        ResoniteMod.Error("Gaze tracking is not allowed! Please enable it in the Varjo Base!");
+                        return false;
+                    }
+                    varjo_GazeInit(_session);
+                    varjo_SyncProperties(_session);
+                    return true;
+                }
+                // Aand we don't need the evil hook anymore either
+                hook.ThreadACL.SetInclusiveACL(new Int32[] { });
+            }
             return false;
         }
-        if (!varjo_IsGazeAllowed(_session))
+
+        public void Teardown()
         {
-            UniLog.Error("Gaze tracking is not allowed! Please enable it in the Varjo Base!");
-            return false;
+            varjo_SessionShutDown(_session);
         }
-        varjo_GazeInit(_session);
-        varjo_SyncProperties(_session);
-        return true;
-    }
 
-    public override void Update()
-    {
-        if (_session == IntPtr.Zero)
-            return;
-
-        // Get's GazeData and EyeMeasurements from the Varjo SDK
-        // Return value states whether or not the request was successful (true = has Data; false = Error occured)
-        bool hasData = varjo_GetGazeData(_session, out gazeData, out eyeMeasurements);
-
-        if (!hasData)
-            UniLog.Log("Error while getting Gaze Data");
-    }
-
-    public override void Teardown() { }
-
-    public override string GetName() => "Varjo Native Interface";
-
-    private bool LoadLibrary()
-    {;
-        var path = "VarjoLib.dll";
-        if (LoadLibrary(path) == IntPtr.Zero)
+        // Get's the newest Data from the SDK and stores it internally
+        public bool Update()
         {
-            UniLog.Error(string.Concat("Unable to load library ", path));
-            return false;
+            if (_session == IntPtr.Zero)
+                return false;
+
+            // Gets GazeData and EyeMeasurements from the Varjo SDK
+            // Return value states whether or not the request was successful (true = has Data; false = Error occured)
+            bool hasData = varjo_GetGazeData(_session, out gazeData, out eyeMeasurements);
+
+            if (!hasData)
+                ResoniteMod.Warn("Error while getting Gaze Data");
+
+            return hasData;
         }
-        UniLog.Log(string.Concat("Loaded library ", path));
-        return true;
+        #endregion
+
+        #region Public Getters
+        public GazeData GetGazeData()
+        {
+            return gazeData;
+        }
+
+        public EyeMeasurements GetEyeMeasurements()
+        {
+            return eyeMeasurements;
+        }
+
+        public string GetHMDName()
+        {
+            int bufferSize = varjo_GetPropertyStringSize(_session, VarjoPropertyKey.HMDProductName);
+            StringBuilder buffer = new StringBuilder(bufferSize);
+            varjo_GetPropertyString(_session, VarjoPropertyKey.HMDProductName, buffer, bufferSize);
+
+            return buffer.ToString();
+        }
+        #endregion
+
+        #region Internal helper methods
+        private bool LoadLibrary()
+        {
+            var path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\TrackingLibs\\VarjoLib.dll";
+            if (path == null)
+            {
+                ResoniteMod.Error(string.Concat("Couldn't extract the library ", path));
+                return false;
+            }
+            if (LoadLibrary(path) == IntPtr.Zero)
+            {
+                ResoniteMod.Error(string.Concat("Unable to load library ", path));
+                return false;
+            }
+            ResoniteMod.Msg(string.Concat("Loaded library ", path));
+            return true;
+        }
+
+        private static bool VarjoAvailable()
+        {
+            // totally not how the official Varjo library works under the hood
+            return File.Exists("\\\\.\\pipe\\Varjo\\InfoService");
+        }
+
+
+        //Our actual hooked handler function
+        UInt32 GetCurrentProcessId_Hook()
+        {
+            // We try to only intercept calls from the Varjo client.
+            // We can't have a _ReturnAddress() intrinsic in C# so we'll use the EasyHook's method. I promise it'll only be called a few times
+            IntPtr returnAddr = HookRuntimeInfo.ReturnAddress;
+            IntPtr callerModule;
+            if (GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, returnAddr, out callerModule))
+            {
+                IntPtr libVarjo = GetModuleHandleA("VarjoLib.dll");
+                IntPtr libVarjoRuntime = GetModuleHandleA("VarjoRuntime.dll");
+                if (callerModule == libVarjo || callerModule == libVarjoRuntime)
+                {
+                    // return bogus process id
+                    ResoniteMod.Warn("Hijacked Varjo's process ID call! All good");
+                    return GetCurrentProcessId() + 42;
+                }
+            }
+            return GetCurrentProcessId();
+        }
+
+
+        #endregion
+
+        #region DllImports
+        [DllImport("kernel32", CharSet = CharSet.Unicode, ExactSpelling = false, SetLastError = true)]
+        private static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern bool varjo_IsAvailable();
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern IntPtr varjo_SessionInit();
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern void varjo_SessionShutDown(IntPtr session);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern void varjo_GazeInit(IntPtr session);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern int varjo_GetError(IntPtr session);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern string varjo_GetErrorDesc(int errorCode);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern bool varjo_IsGazeAllowed(IntPtr session);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern bool varjo_IsGazeCalibrated(IntPtr session);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern GazeData varjo_GetGaze(IntPtr session);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern bool varjo_GetGazeData(IntPtr session, out GazeData gaze, out EyeMeasurements eyeMeasurements);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern void varjo_RequestGazeCalibration(IntPtr session);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern bool varjo_GetPropertyBool(IntPtr session, VarjoPropertyKey propertyKey);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern int varjo_GetPropertyInt(IntPtr session, VarjoPropertyKey propertyKey);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Ansi)]
+        private static extern void varjo_GetPropertyString(IntPtr session, VarjoPropertyKey propertyKey, StringBuilder buffer, int bufferSize);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern int varjo_GetPropertyStringSize(IntPtr session, VarjoPropertyKey propertyKey);
+
+        [DllImport("VarjoLib", CharSet = CharSet.Auto)]
+        private static extern void varjo_SyncProperties(IntPtr session);
+
+        #endregion
+
+        #region Hooking related imports and structs
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern UInt32 GetCurrentProcessId();
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+        delegate UInt32 GetCurrentProcessIdDelegate();
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern IntPtr GetModuleHandleA(string lpModuleName);
+
+        [DllImport("kernel32.dll")]
+        static extern bool GetModuleHandleExA(int dwFlags, IntPtr ModuleName, out IntPtr phModule);
+
+        #endregion
+
     }
-
-    [DllImport("kernel32", CharSet = CharSet.Unicode, ExactSpelling = false, SetLastError = true)]
-    private static extern IntPtr LoadLibrary(string lpFileName);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern bool varjo_IsAvailable();
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern IntPtr varjo_SessionInit();
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern void varjo_SessionShutDown(IntPtr session);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern void varjo_GazeInit(IntPtr session);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern int varjo_GetError(IntPtr session);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern string varjo_GetErrorDesc(int errorCode);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern bool varjo_IsGazeAllowed(IntPtr session);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern bool varjo_IsGazeCalibrated(IntPtr session);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern GazeData varjo_GetGaze(IntPtr session);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern bool varjo_GetGazeData(IntPtr session, out GazeData gaze, out EyeMeasurements eyeMeasurements);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern void varjo_RequestGazeCalibration(IntPtr session);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern bool varjo_GetPropertyBool(IntPtr session, int propertyKey);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern int varjo_GetPropertyInt(IntPtr session, int propertyKey);
-
-    [DllImport("VarjoLib", CharSet = CharSet.Auto)]
-    private static extern void varjo_SyncProperties(IntPtr session);
 }
